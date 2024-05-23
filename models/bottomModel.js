8538d815ee1d1d917e70f03a7718a69d9506e3d8
Camille <camille.soliman@rfsmart.com>
2024-05-23 15:22:07 -0400

    added the transparent images. changed homie to BG image

--- models/bottomModel.js
+++ models/bottomModel.js
@@ -7,19 +7,19 @@ const bottomsArray = [
     { 
         id: 1,
         name: "Flame Resistant Pants",
-        url: "https://i5.walmartimages.com/seo/Flame-Resistant-FR-High-Visibility-Hi-Vis-88-C-12-N-Pant-Trouser-32W-x-32L-Khaki_12fd4e78-9937-4848-8628-884dbb9ebb33.bde7d3a76e05c2f6c1a87f17b09ff710.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF"
+        url: "https://i.imgur.com/dQOnCxA.png"
     },
 
     { 
         id: 2,
         name: "Navy Blue Pants",
-        url: "https://i5.walmartimages.com/asr/6a1bf5de-a50a-4626-a18d-470aa880f948.7c9c090e5d4abb772fa530f6e21a49a7.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF"
+        url: "https://i.imgur.com/O89b3R4.png"
     },
 
     { 
         id: 3,
         name: "Pikachu Pants",
-        url: "https://i5.walmartimages.com/asr/8bcc7185-2dc2-4158-bca0-a34fc871adfc.920bf13dd14944e71b26aa9f72056f4d.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF"
+        url: "https://i.imgur.com/vXTRM0u.png"
     }
 ];
 
--- models/topModel.js
+++ models/topModel.js
@@ -2,24 +2,24 @@ const topsArray = [
     {
         id: 0,
         name: "None",
-        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7e-2l-zHK44dUf7vkbA-qfttngGAx1FPbvkwaawqJiw&s"
+        url: ""
     },
     { 
         id: 1,
         name: "Android User",
-        url: "https://i5.walmartimages.com/asr/67c5a39f-2b98-4a8e-aaf1-3fc10451fa1c.8581e561c257e53eb66c1c83bae4f9cb.jpeg?odnHeight=640&odnWidth=640&odnBg=FFFFFF"
+        url: "https://i.imgur.com/sIgZ5a4.png"
     },
 
     { 
         id: 2,
         name: "Teal?",
-        url: "https://i5.walmartimages.com/seo/Hanes-Men-s-and-Big-Men-s-Authentic-Short-Sleeve-Tee-Up-To-Size-6XL_a468aec2-57ba-40f2-8768-313a28dfe4d9_1.fa31d12f169b4b01ec4e8f5eefea21c6.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF"
+        url: "https://i.imgur.com/toEcNfH.png"
     },
 
     { 
         id: 3,
         name: "Software Developer Hoodie",
-        url: "https://i5.walmartimages.com/seo/Gildan-Unisex-Heavy-Blend-Fleece-Full-Zip-Hooded-Sweatshirt-Size-Small-to-3XL_74b7acd4-cb35-4b75-9507-90f82f7d625a.c9c31df9e37318a408ffa54d3db54183.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF"
+        url: "https://i.imgur.com/pR8eB9h.png"
     }
 ];
 
@@ -33,5 +33,6 @@ function getAll(){
 
 function getOne(id){
     const top = topsArray.find((top)=> top.id === parseInt(id));
+    console.log(top);
     return top; 
 };
